59070f8a740f0d4d2ebc0647385a4cea8c8b00d6
399800834 <399800834@qq.com>
2018-07-05 09:19:37 +0800

    second

---
+++ new_move/js/perform.js
@@ -0,0 +1,10 @@
+new WOW().init();
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'vertical',
+    loop:true,
+    effect:'fade',
+    speed:700,
+    autoplay:3000
+});
